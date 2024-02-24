cd305ca42beef2671faeac3c1ff6b594efb2481b
SURYA U <159547373+suryafsdsurya@users.noreply.github.com>
2024-02-24 10:55:52 +0530

    Add files via upload

---
+++ COUNTYR DETAILS/script.js
@@ -0,0 +1,15 @@
+// creating an XML HTTP Request object
+const req = new XMLHttpRequest();
+// what should i do and From where i have to connect
+req.open("GET", "https://restcountries.com/v3.1/all");
+req.send(); // sending a request to the server
+// what to do when the request is completed successfully
+req.onload = function () {
+  const obj = JSON.parse(this.response);
+  for (let ind = 0; ind < obj.length; ind++) {
+    console.log(obj[ind].name);
+    console.log(obj[ind].region);
+    console.log(obj[ind].subregion);
+    console.log(obj[ind].population);
+  }
+};
