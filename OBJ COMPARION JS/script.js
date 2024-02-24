ec970654276a51ab46316f3d47f79c9c0e062b4d
SURYA U <159547373+suryafsdsurya@users.noreply.github.com>
2024-02-24 11:18:32 +0530

    Add files via upload

---
+++ OBJ COMPARION JS/script.js
@@ -0,0 +1,21 @@
+//How to compare two JSON have the same properties without order
+//let obj1 = {name:"person1", age:5};
+//ler obj2 = {age:5 , name:"person1"};
+
+let obj1 = { name: "person1", age: 5 };
+let obj2 = { age: 5, name: "person1" };
+
+var assumption = true;
+
+if (Object.keys(obj1).length == Object.keys(obj2).length) {
+  for (let key in obj1) {
+    if (obj1[key] == obj2[key]) {
+      continue;
+    } else {
+      assumption = false;
+      break;
+    }
+  }
+}
+
+console.log(assumption);
